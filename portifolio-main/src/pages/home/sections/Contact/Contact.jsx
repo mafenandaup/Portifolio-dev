288e1aeaeef7f285a4080611591cd16380d81052
Maria Fernanda <mafenandaup@gmail.com>
2025-04-08 20:33:14 -0300

    refactor: contact

---
+++ portifolio-main/src/pages/home/sections/Contact/Contact.jsx
@@ -0,0 +1,13 @@
+import './Contact.css'
+
+function Contact() {
+    return (
+        <>
+            <footer>
+<h1>Saiba onde me encontrar:</h1>
+            </footer>
+        </>
+    );
+}
+
+export default Contact;
