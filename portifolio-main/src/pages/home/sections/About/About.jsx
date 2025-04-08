239563644293bd5d980207ebbabeac5889850f59
Maria Fernanda <mafenandaup@gmail.com>
2025-04-08 19:59:38 -0300

    refactor: modify about and import img

--- portifolio-main/src/pages/home/sections/About/About.jsx
+++ portifolio-main/src/pages/home/sections/About/About.jsx
@@ -1,12 +1,14 @@
 import './About.css'
+import iconImage from './assets/imgs/icon.jpg';
 
 function About() {
     return (
         <>
             <section>
-                
+                <img src="assets\imgs\icon.jpg" alt="" />
                 <article>
                     <h1>Quem Sou Eu?</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident voluptatem nihil perspiciatis impedit est necessitatibus hic! Quis, officia unde molestiae ad architecto explicabo cumque id neque iure recusandae dignissimos laborum.</p>
                 </article>
             </section>
         </>
