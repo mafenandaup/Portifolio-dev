263dbb2902e93ad3d991ba6d1565e2a56f9f0590
Maria Fernanda <mafenandaup@gmail.com>
2025-04-14 17:10:48 -0300

    refactor: contact divs

--- portifolio-main/src/pages/home/sections/Contact/Contact.jsx
+++ portifolio-main/src/pages/home/sections/Contact/Contact.jsx
@@ -4,17 +4,15 @@ function Contact() {
     return (
         <>
             <footer>
-                <div className='divisors'>
                     <div className='social-media'>
                         <h1>Saiba onde me encontrar:</h1>
-                    </div>
-                    <div className='social-media'>
-                        <h1>Saiba onde me encontrar:</h1>
-                    </div>
-                    <div className='social-media'>
-                        <h1>Saiba onde me encontrar:</h1>
-                    </div>
                 </div>
+                <div className='social-media'>
+                        <h1>Saiba onde me encontrar:</h1>
+                </div>
+                <div className='social-media'>
+                        <h1>Saiba onde me encontrar:</h1>
+                    </div>
             </footer>
         </>
     );
