dd0e05922029e9b04c218343c3ec28e1414b27cf
Maria Fernanda <mafenandaup@gmail.com>
2025-04-14 15:06:08 -0300

    feat: skills and projects components

---
+++ portifolio-main/src/pages/home/sections/Skills/Contact.jsx
@@ -0,0 +1,23 @@
+import './Contact.css'
+
+function Contact() {
+    return (
+        <>
+            <footer>
+                <div className='divisors'>
+                    <div className='social-media'>
+                        <h1>Saiba onde me encontrar:</h1>
+                    </div>
+                    <div className='social-media'>
+                        <h1>Saiba onde me encontrar:</h1>
+                    </div>
+                    <div className='social-media'>
+                        <h1>Saiba onde me encontrar:</h1>
+                    </div>
+                </div>
+            </footer>
+        </>
+    );
+}
+
+export default Contact;
