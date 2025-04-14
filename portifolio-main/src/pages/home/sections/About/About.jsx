6b30af64e6c387fb7ba47167b23940f050950264
Maria Fernanda <mafenandaup@gmail.com>
2025-04-14 19:04:32 -0300

    refactor: edit text

--- portifolio-main/src/pages/home/sections/About/About.jsx
+++ portifolio-main/src/pages/home/sections/About/About.jsx
@@ -8,7 +8,8 @@ function About() {
                 <img src={iconImage} alt="foto da autora" />
                 <article>
                     <h1>Além do Código</h1>
-                    <p>Maria Fernanda, Front-end Developer</p>
+                    <p>Maria Fernanda, Front-end Developer</p><br />
+                    <p>hello</p>
                 </article>
             </section>
         </>
