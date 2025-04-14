0f4f73464b4babc6092f8595dcbca639f97ed5a4
Maria Fernanda <mafenandaup@gmail.com>
2025-04-14 10:46:47 -0300

    refactor:local styles

--- portifolio-main/src/pages/home/sections/Contact/Contact.jsx
+++ portifolio-main/src/pages/home/sections/Contact/Contact.jsx
@@ -4,8 +4,16 @@ function Contact() {
     return (
         <>
             <footer>
-                <div className='social-media'>
-                    <h1>Saiba onde me encontrar:</h1>
+                <div className='divisors'>
+                    <div className='social-media'>
+                        <h1>Saiba onde me encontrar:</h1>
+                    </div>
+                    <div className='social-media'>
+                        <h1>Saiba onde me encontrar:</h1>
+                    </div>
+                    <div className='social-media'>
+                        <h1>Saiba onde me encontrar:</h1>
+                    </div>
                 </div>
             </footer>
         </>
