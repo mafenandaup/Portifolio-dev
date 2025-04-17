9e26edf5b2d21fcd6660e002bbc15308ce2c3ffa
Maria Fernanda <mafenandaup@gmail.com>
2025-04-17 16:06:59 -0300

    refactor: change logo and hover fx

--- portifolio-main/src/pages/home/sections/Contact/Contact.jsx
+++ portifolio-main/src/pages/home/sections/Contact/Contact.jsx
@@ -1,18 +1,26 @@
 import './Contact.css'
+import GHLogo from "../../../../assets/imgs/githubicon.png";
+import LKDLogo from "../../../../assets/imgs/linkedinicon.png";
+import EMAILLogo from "../../../../assets/imgs/emailicon.png";
 
 function Contact() {
     return (
         <>
             <footer>
-                    <div className='social-media'>
-                        <h1>Saiba onde me encontrar:</h1>
-                </div>
                 <div className='social-media'>
-                        <h1>Saiba onde me encontrar:</h1>
-                </div>
-                <div className='social-media'>
-                        <h1>Saiba onde me encontrar:</h1>
+                    <h1>Saiba onde me encontrar:</h1>
+                    <div className='logos'>
+                        <img src={LKDLogo} className='logo-adjust' alt="lINKEDIN-LOGO" />
+                        <img src={GHLogo} className='logo-adjust' alt="GITHUB-LOGO" />
+                        <img src={EMAILLogo} className='logo-adjust' alt="EMAIL-LOGO" />
                     </div>
+                </div>
+                <div className='social-media'>
+                    <h1>Saiba onde me encontrar:</h1>
+                </div>
+                <div className='social-media'>
+                    <h1>Saiba onde me encontrar:</h1>
+                </div>
             </footer>
         </>
     );
