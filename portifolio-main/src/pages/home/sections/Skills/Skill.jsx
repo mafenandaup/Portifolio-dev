1640acb9c8a6c34d88578bed35e27dd36bfb14d2
Maria Fernanda <mafenandaup@gmail.com>
2025-04-25 21:29:56 -0300

    refactor: finalmente consegui ajustar as imagens

--- portifolio-main/src/pages/home/sections/Skills/Skill.jsx
+++ portifolio-main/src/pages/home/sections/Skills/Skill.jsx
@@ -1,9 +1,8 @@
 export function Skill({ title, image }) {
     return (
-      <div className="skill-item">
-        <img src={image} alt={`${title} logo`} className="skill-icon" />
-        <h2>{title}</h2>
-      </div>
+        <div className="skill-item">
+            <img src={image} alt={`${title} logo`} className="skill-icon" />
+            <h2>{title}</h2>
+        </div>
     );
-  }
-  
+}
--- portifolio-main/src/pages/home/sections/Skills/Skills.jsx
+++ portifolio-main/src/pages/home/sections/Skills/Skills.jsx
@@ -5,14 +5,12 @@ import { skill } from './Skills.json'
 function Skills() {
     return (
         <>
-            <section className='section-skills' id='skills'>
+            <section className="section-skills" id="skills">
                 <h1>Minhas habilidades:</h1>
-                <div className='skills-cont'>
-                    {skill.map((skill) => {
-                        return (
-                            <Skill key={skill.title} title={skill.title} img={skill.img}></Skill>
-                        )
-                    })}
+                <div className="skills-cont">
+                    {skill.map((skill) => (
+                        <Skill key={skill.title} title={skill.title} image={skill.image} />
+                    ))}
                 </div>
             </section>
         </>
