7caa52aa6625ec7c1ae09a03d816052c0ab050f8
Maria Fernanda <mafenandaup@gmail.com>
2025-04-28 14:56:37 -0300

    refactor: animations contact

--- portifolio-main/src/pages/home/sections/Contact/Contact.jsx
+++ portifolio-main/src/pages/home/sections/Contact/Contact.jsx
@@ -5,6 +5,8 @@ import LKDLogo from "../../../../assets/imgs/linkedinicon.png";
 import EMAILLogo from "../../../../assets/imgs/emailicon.png";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+// eslint-disable-next-line no-unused-vars
+import { motion, scale, stagger } from "framer-motion";
 
 
 function Contact() {
@@ -14,25 +16,25 @@ function Contact() {
                 <div className='social-media'>
                     <h1>Saiba onde me encontrar</h1>
                     <div className='logos'>
-                        <a href="https://www.linkedin.com/in/mariafpm/" target="_blank" aria-label='Linkedin' rel="noopener noreferrer"><img src={LKDLogo} className='logo-adjust' alt="lINKEDIN-LOGO" /></a>
-                        <a href="https://github.com/mafenandaup" target="_blank" aria-label='Github' rel="noopener noreferrer"><img src={GHLogo} className='logo-adjust' alt="GITHUB-LOGO" /></a>
-                        <a href="mailto:mariafernandapmaia@gmail.com" class="fas fa-envelope" aria-label="Mail" target="_blank"><img src={EMAILLogo} className='logo-adjust' alt="EMAIL-LOGO" /></a>
+                        <motion.a whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} href="https://www.linkedin.com/in/mariafpm/" target="_blank" aria-label='Linkedin' rel="noopener noreferrer"><img src={LKDLogo} className='logo-adjust' alt="lINKEDIN-LOGO" /></motion.a >
+                        <motion.a whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} href="https://github.com/mafenandaup" target="_blank" aria-label='Github' rel="noopener noreferrer"><img src={GHLogo} className='logo-adjust' alt="GITHUB-LOGO" /></motion.a >
+                        <motion.a whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} href="mailto:mariafernandapmaia@gmail.com" class="fas fa-envelope" aria-label="Mail" target="_blank"><img src={EMAILLogo} className='logo-adjust' alt="EMAIL-LOGO" /></motion.a >
                     </div>
                 </div>
                 <div className='social-media'>
                     <h1>Quick Links</h1>
-                    <a href='#projects'>
+                    <motion.a  whileTap={{ scale: 0.9 }} href='#projects'>
                         <FontAwesomeIcon icon={faAngleRight} className='arrow-icon' />  Projetos
-                    </a>
-                    <a href='#about'>
+                    </motion.a >
+                    <motion.a  whileTap={{ scale: 0.9 }} href='#about'>
                         <FontAwesomeIcon icon={faAngleRight} className='arrow-icon' />  Sobre Mim
-                    </a>
-                    <a href='#home'>
+                    </motion.a >
+                    <motion.a  whileTap={{ scale: 0.9 }} href='#home'>
                         <FontAwesomeIcon icon={faAngleRight} className='arrow-icon' />  Home
-                    </a>
-                    <a href='#skills'>
+                    </motion.a >
+                    <motion.a  whileTap={{ scale: 0.9 }} href='#skills'>
                         <FontAwesomeIcon icon={faAngleRight} className='arrow-icon' />  Skills
-                    </a>
+                    </motion.a >
                 </div>
 
                 <div className='social-media' id='copyright'>
