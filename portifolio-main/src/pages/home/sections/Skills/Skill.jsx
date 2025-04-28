4b862b8915cdfecf9fae6d188ef849e5a6f6e563
Maria Fernanda <mafenandaup@gmail.com>
2025-04-28 14:18:10 -0300

    refactor: skill-item animation

--- portifolio-main/src/pages/home/sections/Skills/Skill.jsx
+++ portifolio-main/src/pages/home/sections/Skills/Skill.jsx
@@ -1,10 +1,11 @@
-
+// eslint-disable-next-line no-unused-vars
+import { motion, scale, stagger } from "framer-motion";
 
 export function Skill({ title, image }) {
     return (
-        <div className="skill-item">
+        <motion.div whileHover={{scale: 1.04} } className="skill-item">
             <img src={image} alt={`${title} logo`} className="skill-icon" />
             <h2>{title}</h2>
-        </div>
+        </motion.div>
     );
 }
--- portifolio-main/src/pages/home/sections/Skills/Skills.jsx
+++ portifolio-main/src/pages/home/sections/Skills/Skills.jsx
@@ -1,14 +1,15 @@
 import './Skills.css'
 import { Skill } from './Skill'
 import { skill } from './Skills.json'
-import { motion, scale } from "framer-motion";
+// eslint-disable-next-line no-unused-vars
+import { motion, scale, stagger } from "framer-motion";
+
 
 function Skills() {
     return (
         <>
             <section className="section-skills" id="skills">
                 <h1>Minhas habilidades:</h1>
-                <motion.p whileHover={{ scale: 1.3 }}>try me</motion.p>
                 <div className="skills-cont">
                     {skill.map((skill) => (
                         <Skill key={skill.title} title={skill.title} image={skill.image} />
