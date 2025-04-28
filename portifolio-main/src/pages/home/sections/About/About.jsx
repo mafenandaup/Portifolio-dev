e7efadb102c0793fa1dd061c21ec7783b053facd
Maria Fernanda <mafenandaup@gmail.com>
2025-04-28 19:54:06 -0300

    refactor: transition title

--- portifolio-main/src/pages/home/sections/About/About.jsx
+++ portifolio-main/src/pages/home/sections/About/About.jsx
@@ -12,6 +12,18 @@ const imgTransition = {
     exit: { opacity: 0, x: 50, transition: { duration: 0.4 } }, 
 };
 
+const titleTransition = {
+    hidden: { opacity: 0, y: -50 },
+    visible: { opacity: 1, y: 0, transition: { duration: 1.3} },
+    exit: { opacity: 0, y: 50, transition: { duration: 0.4 } }, 
+};
+
+const textTransition = {
+    hidden: { opacity: 0, x: 50 },
+    visible: { opacity: 1, x: 0, transition: { duration: 1.3} },
+    exit: { opacity: 0, x: -50, transition: { duration: 0.4 } }, 
+};
+
 function About() {
     const ref = useRef(null); // Referência ao elemento
     const isInView = useInView(ref, { once: true }); // Detecta quando o elemento entra na viewport
@@ -21,11 +33,11 @@ function About() {
             <section id='about'>
                 <motion.img ref={ref} variants={imgTransition} initial="hidden" animate={isInView ? "visible" : "hidden"} exit= "exit"src={iconImage} alt="foto da autora" className='autor-img' />
                 <article>
-                    <h1>Além do Código</h1>
-                    <p>Maria Fernanda, Front-end Developer</p><br />
-                    <p>Graduanda em Engenharia de Software pela UCSAL, sou uma profissional imersa na busca por ressonância entre design e funcionalidade, priorizando a simplicidade e a excelência na experiência do usuário.</p><br />
-                    <p>Meu foco é entregar projetos que sejam não só úteis, mas que também tenham uma estética refinada, intuitiva e coesão nos elementos visuais. </p><br />
-                    <p>Seja resolvendo problemas complexos ou me expressando criativamente, estou sempre aprendendo e cada vez mais me aprimorando como profissional. </p>
+                    <motion.h1 ref={ref} variants={titleTransition} initial="hidden" animate={isInView ? "visible" : "hidden"} exit= "exit">Além do Código</motion.h1>
+                    <motion.p ref={ref} variants={textTransition} initial="hidden" animate={isInView ? "visible" : "hidden"} exit= "exit">Maria Fernanda, Front-end Developer</motion.p><br />
+                    <motion.p ref={ref} variants={textTransition} initial="hidden" animate={isInView ? "visible" : "hidden"} exit= "exit">Graduanda em Engenharia de Software pela UCSAL, sou uma profissional imersa na busca por ressonância entre design e funcionalidade, priorizando a simplicidade e a excelência na experiência do usuário.</motion.p><br />
+                    <motion.p ref={ref} variants={textTransition} initial="hidden" animate={isInView ? "visible" : "hidden"} exit= "exit">Meu foco é entregar projetos que sejam não só úteis, mas que também tenham uma estética refinada, intuitiva e coesão nos elementos visuais. </motion.p><br />
+                    <motion.p ref={ref} variants={textTransition} initial="hidden" animate={isInView ? "visible" : "hidden"} exit= "exit">Seja resolvendo problemas complexos ou me expressando criativamente, estou sempre aprendendo e cada vez mais me aprimorando como profissional. </motion.p>
                 </article>
 
             </section>
