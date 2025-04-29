bcf81355d080fa8541c907aad050afe3d83dfa02
Maria Fernanda <mafenandaup@gmail.com>
2025-04-29 19:29:33 -0300

    refactor: responsiveness "about" component

--- portifolio-main/src/pages/home/sections/Projects/Projects.jsx
+++ portifolio-main/src/pages/home/sections/Projects/Projects.jsx
@@ -2,23 +2,15 @@ import { Project } from './Project';
 import './Projects.css'
 import { project } from './Projects.json'
 import React from 'react';
-import { motion, scale, stagger } from "framer-motion";
+// eslint-disable-next-line no-unused-vars
+import { motion, scale,  } from "framer-motion";
 
 const skillVariants = {
-    hidden: { opacity: 0 },
-    visible: {
-        opacity: 1,
-        transition: {
-            staggerChildren: 0.1,
-        },
-    },
+    hidden: { opacity: 0, y: 50 }, 
+    visible: { opacity: 1, y: 0,  transition: { duration: 0.9 }, },
+    exit: { opacity: 0, y: -50, transition: { duration: 0.4 } }, 
 };
 
-const itemVariants = {
-    hidden: { opacity: 0, y: 50 },
-    visible: { opacity: 1, y: 0, transition: { duration: 0.6 } },
-    exit: { opacity: 0, y: -50, transition: { duration: 0.4 } },
-};
 
 function Projects() {
     return (
@@ -26,15 +18,10 @@ function Projects() {
 
             <section className='section-projects' id='projects'>
                 <h1>Meus projetos:</h1>
-                <motion.div className="project-cont"
-                    variants={skillVariants}
-                    initial="hidden"
-                    whileInView="visible"
-                    exit="hidden"
-                    viewport={{ once: true, amount: 0.4 }}>
-                    {project.map((project) => (
-                        <Project key={project.title} title={project.title} img={project.img} desc={project.desc} srccode={project.srccode} ytlink={project.ytlink} />
-                    ))}
+                <motion.div className="project-cont" variants={skillVariants} initial="hidden" whileInView="visible" exit="exit"viewport={{ once: true, amount: 0.4 }}>
+                {project.map((project) => (
+                    <Project key={project.title} title={project.title} img={project.img} desc={project.desc} srccode={project.srccode} ytlink={project.ytlink}/>
+                ))}
                 </motion.div>
             </section>
         </>
